2ab5db294c9aefa511f3e4f88596e8c77bdcee54
Miyuki <yuzh7799@gmail.com>
2022-11-17 00:11:58 +0800

    创建核心模块

---
+++ MiyukiRPC-core/src/main/java/com/miyuki/mrpc/core/server/Server.java
@@ -0,0 +1,19 @@
+package com.miyuki.mrpc.core.server;
+
+import io.netty.channel.EventLoopGroup;
+
+import java.util.logging.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: miyuki
+ * @Date: 2022/11
+ * @Description:
+ */
+public class Server {
+    private static final EventLoopGroup bossGroup = null;
+
+    private static EventLoopGroup workerGroup = null;
+    private 
+}
