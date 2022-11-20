3b41f266be41935c57c472c34cc8ff13d83e8e1f
Miyuki <yuzh7799@gmail.com>
2022-11-20 22:30:32 +0800

    ZkServiceProvider完成，发布，注册服务

--- MiyukiRPC-core/src/main/java/com/miyuki/mrpc/core/provider/impl/ZkServiceProvider.java
+++ MiyukiRPC-core/src/main/java/com/miyuki/mrpc/core/provider/impl/ZkServiceProvider.java
@@ -1,12 +1,22 @@
 package com.miyuki.mrpc.core.provider.impl;
 
+import com.miyuki.mrpc.core.common.enums.RpcErrorMessageEnum;
+import com.miyuki.mrpc.core.common.exception.RpcException;
+import com.miyuki.mrpc.core.common.extension.ExtensionLoader;
 import com.miyuki.mrpc.core.config.RpcServiceConfig;
 import com.miyuki.mrpc.core.provider.ServiceProvider;
+import com.miyuki.mrpc.core.register.ServiceRegistry;
+import com.miyuki.mrpc.core.register.zk.ServiceRegisterImpl;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.imageio.spi.ServiceRegistry;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.miyuki.mrpc.core.common.cache.CommonServerCache.SERVER_CONFIG;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,25 +27,47 @@ import java.util.Set;
  */
 @Slf4j
 public class ZkServiceProvider implements ServiceProvider {
-
-    private final Map<String, Object> serviceMap;
-
+    private final Map<String,Object> serviceMap;
     private final Set<String> registeredService;
-
     private final ServiceRegistry serviceRegistry;
+
+    private ZkServiceProvider(){
+        serviceMap = new ConcurrentHashMap<>();
+        registeredService = ConcurrentHashMap.newKeySet();
+        serviceRegistry = ExtensionLoader.getExtensionLoader(ServiceRegistry.class).getExtension(SERVER_CONFIG.getRegisterType());
+    }
 
     @Override
     public void addService(RpcServiceConfig rpcServiceConfig) {
+        String rpcServiceName = rpcServiceConfig.getRpcServiceName();
+        if (registeredService.contains(rpcServiceName)) return;
+        registeredService.add(rpcServiceName);
+        System.out.println(Thread.currentThread().getName());
+        serviceMap.put(rpcServiceName,rpcServiceConfig.getService());
+        log.info("Add service: {} and interfaces:{}", rpcServiceName, rpcServiceConfig.getService().getClass().getInterfaces());
 
     }
 
     @Override
     public Object getService(String rpcServiceName) {
-        return null;
+        System.out.println(Thread.currentThread().getName());
+        Object service = serviceMap.get(rpcServiceName);
+        if (null == service){
+            throw new RpcException(RpcErrorMessageEnum.SERVICE_CAN_NOT_BE_FOUND);
+        }
+        return service;
     }
 
     @Override
     public void publishService(RpcServiceConfig rpcServiceConfig) {
+        try {
+            String host = InetAddress.getLocalHost().getHostAddress();
+            this.addService(rpcServiceConfig);
+            serviceRegistry.registerService(new InetSocketAddress(host, SERVER_CONFIG.getServerPort()),rpcServiceConfig);
+
+        } catch (UnknownHostException e) {
+            log.error("occur exception when getHostAddress", e);
+        }
 
     }
 }
